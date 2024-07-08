d659b2a9117d8edbd0ae592ae01806b4ea3105f7
Isilud <jolan.houot@gmail.com>
2024-07-08 14:30:47 +0200

    add test profile

--- application/src/test/java/com/paymybuddy/application/integration/controller/UserControllerTest.java
+++ application/src/test/java/com/paymybuddy/application/integration/controller/UserControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +29,7 @@ import com.paymybuddy.application.repository.UserRepository;
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 @Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class UserControllerTest {
 
         @Autowired
